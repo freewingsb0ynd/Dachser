554d255e744c0dd434eeac3775f2540aeac3ba7d
Hoangelato <lhp.huyhoangnguyen@gmail.com>
2016-11-20 03:12:15 +0700

    fixed conveyor

---
+++ src/GameObject/ConveyorFixed.java
@@ -0,0 +1,10 @@
+package GameObject;
+
+/**
+ * Created by Hoangelato on 20/11/2016.
+ */
+public class ConveyorFixed extends Conveyor {
+    protected ConveyorFixed(int posX, int posY) {
+        super(posX, posY);
+    }
+}
