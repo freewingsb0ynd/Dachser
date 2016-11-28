0a377385072eaf559eb94a1aefe0a1b0f1eeacfd
Hoangelato <lhp.huyhoangnguyen@gmail.com>
2016-11-28 10:13:08 +0700

    conveyor update

---
+++ src/GameObject/ConveyorSwitch.java
@@ -0,0 +1,11 @@
+package GameObject;
+
+/**
+ * Created by Hoangelato on 22/11/2016.
+ */
+public class ConveyorSwitch extends ConveyorFixed {
+    protected ConveyorSwitch(int posX, int posY) {
+        super(posX, posY);
+    }
+
+}
---
+++ src/GameObject/ConveyorTurn.java
@@ -0,0 +1,10 @@
+package GameObject;
+
+/**
+ * Created by Hoangelato on 22/11/2016.
+ */
+public class ConveyorTurn extends ConveyorFixed {
+    protected ConveyorTurn(int posX, int posY) {
+        super(posX, posY);
+    }
+}
